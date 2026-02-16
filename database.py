e5e9c6b93ac57d430897137b6f9f127793dceafc
Daniel Sebastin A <danielsebastinds@gmail.com>
2026-02-16 22:48:36 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url="postgresql://postgres:Dansasuke12@rdsproj.cnwcewm0mb3c.ap-south-1.rds.amazonaws.com:5432/postgres?sslmode=require"
+db_url="postgresql://username:password@rdsproj.cnwcewm0mb3c.ap-south-1.rds.amazonaws.com:5432/postgres?sslmode=require"
 
 engine=create_engine(db_url,pool_pre_ping=True)
 session=sessionmaker(autocommit=False,autoflush=False,bind=engine)
